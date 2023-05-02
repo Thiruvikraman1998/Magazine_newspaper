6c7a513342e07a07fafd83aa70081070b565fb43
Thiruvikraman1998 <development.thiru@gmail.com>
2023-05-02 16:12:11 +0530

    initial commit

---
+++ magazine_newspaper/lib/screens/stories.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoriesScreen extends StatelessWidget {
+  const StoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Stories Screen"));
+  }
+}
---
+++ magazine_newspaper/lib/screens/unlimited.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UnlimitedScreen extends StatelessWidget {
+  const UnlimitedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Unlimited Screen"));
+    ;
+  }
+}

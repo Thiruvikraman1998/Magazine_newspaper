3fbbcf3124ebe56e38f77e8a8f1fdaf26fa8671a
Thiruvikraman1998 <development.thiru@gmail.com>
2023-05-02 19:12:33 +0530

    commit

---
+++ magazine_newspaper/lib/models/popular_magz.dart
@@ -0,0 +1,17 @@
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// class Popular {
+//   List<Hits>? hits;
+
+//   Popular(this.hits);
+
+//   Popular.fromJson(Map<String, dynamic> Json) {
+//     if (json['hits'] != null) {
+//       hits = <Hits>[];
+//       json['hits'].forEach((v) =>(hits.add(Hits.)))
+//     }
+//   }
+// }
+
+// final String url = "https://sls.magzter.com/maglists/prod/magazine-filter?storeID=1&categoryID=BS&ver=3";
